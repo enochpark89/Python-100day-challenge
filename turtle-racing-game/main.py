f864fc29e050ec585d2f590aca29c9f9906c9bd9
Enoch Park <44038883+enochpark89@users.noreply.github.com>
2022-07-12 20:16:48 -0700

    20220712-turtlegamecompleted

--- turtle-racing-game/main.py
+++ turtle-racing-game/main.py
@@ -1,35 +1,51 @@
+
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 screen = Screen()
 
+# Screen setup
+screen.setup(width=500, height=400)
 
-# Move a turtle forward by 10.
-def move_forwards():
-    tim.forward(10)
+# Pop-up to the user and to enter the text.
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+# color of rainbow in a list
+colors = ["red", "orange", "yellow", "green", "blue", "purple", ]
+turtles = []
 
-def move_backwards():
-    tim.backward(10)
+# Make a list of Turtle objects.
 
-def move_clockwise():
-    tim.right(5)
+# Coordinate of a first turtle.
+x_value = -230
+y_value = -100
+for i in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.color(colors[i])
+    tim.goto(x=x_value, y=y_value)
+    y_value += 30
+    turtles.append(tim)
 
-def move_counter_clockwise():
-    tim.left(5)
+# Make the turtle move forward by random amount
+if user_bet:
+    is_race_on = True
 
-def clear_screen():
-    screen.resetscreen()
+while is_race_on:
 
-screen.listen()
-# Use an event listener
+    # Iterate each turtle and move random distance
+    for turtle in turtles:
+        if turtle.xcor() > 230:
+            is_race_on= False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
 
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
-# Functions as Inputs
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
-screen.onkey(key="a", fun=move_counter_clockwise)
-screen.onkey(key="d", fun=move_clockwise)
-screen.onkey(key="c", fun=clear_screen)
+# Detect the finishline
+
 screen.exitonclick()
-
-# Create a turtle to move in multipile direction
